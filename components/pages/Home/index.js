46ef3bea1fb47153c0f1a4bca4b1b896b33dbb86
greg <ku392000@gmail.com>
2023-04-30 22:14:46 +0800

    Feat: Init homepage component

---
+++ components/pages/Home/index.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button'
+
+const Home = () => {
+  return (
+    <>
+      home
+      <Button variant="contained">Button</Button>
+    </>
+  )
+}
+
+export default Home
